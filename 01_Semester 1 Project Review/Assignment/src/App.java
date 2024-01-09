e7c440ce24502fe3318e70e077c697deced4757c
Keith Owens <owenskei000@warren.k12.in.us>
2024-01-09 10:31:35 -0500

    l

--- 01_Semester 1 Project Review/Assignment/src/App.java
+++ 01_Semester 1 Project Review/Assignment/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Allen likes looking at Florida men");
     }
 }
