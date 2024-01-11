708bf1df0e4bd00aeeb11b48189f75e4c28ad9ea
Keith Owens <owenskei000@warren.k12.in.us>
2024-01-11 10:49:50 -0500

    maths

--- 04_Try Catch Finally/Assignment/src/App.java
+++ 04_Try Catch Finally/Assignment/src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("type your first number");
+        int number1 = scanner.nextInt();
+        System.out.println("type the second number");
+        int number2 = scanner.nextInt();
+
+
+
+        try {
+            int divided = number1 / number2;
+            System.out.println(divided);
+        }catch(ArithmeticException e) {
+            System.out.println("Why you trynna divide by 0 you big idiot. You trynna be like Patrick Star up in here?");
+        }
+        scanner.close();
+
+        
     }
 }
