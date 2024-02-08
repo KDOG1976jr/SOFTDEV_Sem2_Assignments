9b12e549ae4577089e69f7cfce18bda4e9619c34
Keith Owens <owenskei000@warren.k12.in.us>
2024-02-08 10:36:15 -0500

    h

--- 03_File Handling/02_Reading a File/Assignment/src/App.java
+++ 03_File Handling/02_Reading a File/Assignment/src/App.java
@@ -1,5 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String fileName = "src/SampleText16LE.txt";
+
+         FileReader fileReader = new FileReader(fileName);
+         BufferedReader reader = new BufferedReader(fileReader);
+         System.out.println(reader.readLine());
+         System.out.println(reader.readLine());
+         System.out.println(reader.readLine());
+         System.out.println(reader.read());
+
+         FileInputStream fileInputStream = new FileInputStream(fileName);
+         InputStreamReader inputStreamReader = new InputStreamReader(
+                 fileInputStream,
+                 "UTF-16BE");
+         BufferedReader reader2 = new BufferedReader(inputStreamReader);
+
+         System.out.println(reader.readLine());
+         reader.close();
+
+        Path filePath = Paths.get(fileName);
+        List<String> lines = Files.readAllLines(filePath);
+
+        // Loop through the list of lines
+
     }
 }
